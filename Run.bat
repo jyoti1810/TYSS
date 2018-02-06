6cf8a186cfd4de954a852904572e30e3cb2b99b0
dell <dell@DESKTOP-QH08G25>
2018-02-06 18:00:05 +0530

    Hellooo

---
+++ Run.bat
@@ -0,0 +1,1 @@
+java -cp bin com.tyss.Demo  &pause
---
+++ src/com/tyss/Demo.java
@@ -0,0 +1,11 @@
+package com.tyss;
+
+public class Demo 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi welcome to tyss");
+	}
+
+}
